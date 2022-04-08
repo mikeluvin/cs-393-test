fdf0998dab7d12912e8bcc196b5184e13645036d
nicolechen180 <ting-chuchen2023@u.northwestern.edu>
2022-04-08 15:46:03 -0500

    2.3 code

--- Deliverables/2/2.4/frontend.js
+++ Deliverables/2/2.4/frontend.js
@@ -6,29 +6,31 @@ function main() {
     // and https://nodejs.org/api/process.html#processstdin
     process.stdin.resume();
     let inputArr = [];
+    let inputSubArr = [];
     let currString = "";
     process.stdin.on("data", (inputBytes) => {
         let inputString = inputBytes.toString();
         inputString = currString.concat(inputString);
-        let startBracket = 0;
+        let startBracket = inputString.indexOf("{");
         let endBracket = inputString.indexOf("}", startBracket);
 
-        while (inputArr.length < 10 && endBracket != -1) {
+        while (inputSubArr.length < 10 && endBracket != -1) {
             currString = inputString.slice(startBracket, endBracket+1).trim();
-            inputArr.push(JSON.parse(currString));
-            startBracket = endBracket + 1;
+            inputSubArr.push(JSON.parse(currString));
+            startBracket = inputString.indexOf("{", endBracket);
             endBracket = inputString.indexOf("}", startBracket);
             currString = "";
         } 
 
-        /*
+        //
         if (endBracket == -1) {
             currString = inputString.slice(startBracket).trim();
-        } */
+        }
 
-        if (inputArr.length == 10) {
-            process.stdout.write(my_sort(JSON.stringify(inputArr)));
-            process.stdin.destroy();
+        if (inputSubArr.length == 10) {
+            inputArr.push(inputSubArr);
+            inputSubArr = [];
+            //process.stdin.destroy();
         }
     })
 }
