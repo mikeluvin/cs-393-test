1cda5bc87f44e3b7b69c9dde71d5dafbeac2a829
nicolechen180 <ting-chuchen2023@u.northwestern.edu>
2022-05-02 14:42:57 -0500

    6.1 tests

--- Deliverables/create_output.sh
+++ Deliverables/create_output.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-for i in {1..10}
+for i in {1..5}
 do
-    touch "5/5.1/input$i.json"
-    touch "5/5.1/output$i.json"
-    touch "5/5.2/input$i.json"
-    touch "5/5.2/output$i.json"
+    cp "5/5.1/input$((i+5)).json" "6/6.1/input$i.json"
+    cp "5/5.1/output$((i+5)).json" "6/6.1/output$i.json"
 done
